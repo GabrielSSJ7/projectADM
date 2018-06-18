00261970ed69a21a22f684c5f296c5b7148ca575
GabrielSSJ7 <gabriel.n64@hotmail.com>
2018-06-18 13:39:52 -0300

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,22 +8,22 @@
         </div>
         <div class="card-body">
             <div class="row">
-                <div style="padding: 0 5px;" class="col-sm-2">
-                    <a href="{{route('view.produtos')}}" class="btn btn-light ">
+                <div style="padding: 0 5px;" class="col-sm-2 ">
+                    <a href="{{route('view.produtos')}}" class="btn btn-outline-dark dashboard-item">
                         <img src="{{asset('images/warehouse.png')}}" class="img-fluid"><br>
                         <span style="text-align: center">Meus Produtos</span>
                     </a>
                 </div>
 
-                <div style="padding: 0 5px;" class="col-sm-2">
-                    <a href="{{route('view.saida.produto')}}" class="btn btn-light ">
+                <div style="padding: 0 5px;" class="col-sm-2 ">
+                    <a href="{{route('view.saida.produto')}}" class="btn btn-outline-dark dashboard-item">
                         <img src="{{asset('images/money.png')}}" class="img-fluid"><br>
                         <span style="text-align: center">Vender</span>
                     </a>
                 </div>
 
-                <div style="padding: 0 5px;" class="col-sm-2">
-                    <a href="{{route('view.entrada.produto')}}" class="btn btn-light ">
+                <div style="padding: 0 5px;" class="col-sm-2 ">
+                    <a href="{{route('view.entrada.produto')}}" class="btn btn-outline-dark dashboard-item">
                         <img src="{{asset('images/barcode.png')}}" class="img-fluid"><br>
                         <span style="text-align: center">Entrada</span>
                     </a>
